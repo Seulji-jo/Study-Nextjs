413809257ffb2e217b1bcfa3705c5905591dd9de
Seulji-jo <joseuling@gmail.com>
2022-01-02 19:08:30 +0900

    feat: apollo client 세팅

--- freeboard_frontend/pages/_app.js
+++ freeboard_frontend/pages/_app.js
@@ -1,7 +1,20 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const client = new ApolloClient({
+    // 서버 클라이언트 연결
+    uri: 'http://backend03.codebootcamp.co.kr/graphql',
+    // 데이터를 가져오기전에 캐시메모리를 먼저 확인해 동일한 쿼리나 뮤테이션이 있다면 캐시메모리에서 가져옴
+    cache: new InMemoryCache()
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
