75a9c477fc972cf1612183eb4ae517e897fae3ee
Seulji-jo <joseuling@gmail.com>
2022-03-15 10:33:03 +0900

    add: validation 추가

--- freeboard_frontend/src/commons/login/Login.presenter.tsx
+++ freeboard_frontend/src/commons/login/Login.presenter.tsx
@@ -1,21 +1,41 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { Ilogin } from './Login.types';
 import {Background, CloseBtn, SignupForm, Form, InputContainer, Input, ErrorMsg} from '../signup/Signup.style';
 import { Text, Button } from '../styles';
 import {Label, CheckboxContainer, StyledCheckbox, HiddenCheckbox, Icon, SignUpContainer} from './Login.style'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
 import bgImg from '../../../assets/img/signupBg.png';
 import closeBtn from '../../../assets/img/login_cancel.png';
 import Logo from '../../../assets/img/login_logo.png'
-import { Ilogin } from './Login.types';
+
 const LoginPresenter:React.FC<Ilogin> = ({isChecked, handleCheckbox}) => {
+  const schema = yup.object({
+    email: yup.string().required('이메일은 필수 입력입니다.'),
+    password: yup.string().required('비밀번호는 필수 입력입니다.')
+  });
+  const {register, handleSubmit, formState:{errors}} = useForm({
+    resolver:yupResolver(schema)
+  });
+  const onSubmit = data => console.log(data);
   return (
   <Background bgImg={bgImg.src}>
     <CloseBtn btnSrc={closeBtn.src}>close</CloseBtn>
     <SignupForm>
       <Image src={Logo} alt='코드캠프 로고' />
-      <Form>
-        <Input placeholder='이메일을 입력해주세요.'></Input>
-        <Input placeholder='비밀번호를 입력해주세요.'></Input>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <InputContainer>
+          <Input {...register("email")} placeholder='이메일을 입력해주세요.' />
+          {errors?.email?.message && <ErrorMsg>{errors.email?.message}</ErrorMsg>}
+        </InputContainer>
+        <InputContainer>
+          <Input {...register("password")} placeholder='비밀번호를 입력해주세요.' />
+          {errors?.password?.message && <ErrorMsg>{errors.password?.message}</ErrorMsg>}
+        </InputContainer>
+        
         <Label>
           <CheckboxContainer onChange={handleCheckbox}>
             <HiddenCheckbox type={'checkbox'} />
